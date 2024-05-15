701c7eb22bb35b06fae5f855dc0399b1af29a8a9
Loner9 <38490560+loner9@users.noreply.github.com>
2024-05-15 11:06:24 +0700

    Enemy
    Buat musuh, tapi belum kelar

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+    }
+}
