fbc041f17afdb2730c425063bfc2e41e20615733
Loner9 <38490560+loner9@users.noreply.github.com>
2024-05-15 16:17:42 +0700

    Update EnemyPatrol.cs
    finish creating script for enemy patrolling between point a to point b

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -8,6 +8,7 @@ public class EnemyPatrol : MonoBehaviour
     public GameObject pointB;
     private Rigidbody2D rb;
     private Transform currentPoint;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,41 @@ public class EnemyPatrol : MonoBehaviour
     void Update()
     {
         Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
 
+        Debug.Log("posisi " + Vector2.Distance(transform.position, currentPoint.position));
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.8f && currentPoint == pointB.transform)
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.8f && currentPoint == pointA.transform)
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.8f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.8f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 }
